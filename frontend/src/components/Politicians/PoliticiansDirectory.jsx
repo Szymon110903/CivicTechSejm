6081dc1d5ba692a04197128dd101e89654a6a15b
251546 Szymon Kaźmierczak <szymon.kazmierczak60@gmail.com>
2026-07-28 23:27:32 +0200

    routing and core of view

---
+++ frontend/src/components/Politicians/PoliticiansDirectory.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Politicians.css';
+
+const PoliticiansDirectory = () => {
+  return (
+    <div className="politicians-directory-container">
+      <div className="chamber-area">
+        <h2>Sala Sejmowa (w budowie...)</h2>
+      </div>
+      <div className="side-panel-area">
+        <h2>Panel Posła</h2>
+        <p>Szczegóły pojawią się tutaj.</p>
+      </div>
+    </div>
+  );
+};
+
+export default PoliticiansDirectory;
